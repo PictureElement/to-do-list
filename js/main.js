0e2b3c46c3389a370541a731a3ae2ef65736b0dc
Marios Sofokleous <marios.sofokleous@yandex.com>
2017-11-21 00:53:03 +0200

    Add logic that adds a '.rounded' class to the delete button since it is the only button visible in the 'complete' list

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// The event listener:
-//     1. listens to the <button> element
-//     2. listens for a 'click' event
-//     3. performs the following actions when the button is clicked:
+// The event listeners:
+//     1. listen to the specified element
+//     2. listen for a 'click' event
+//     3. perform a series of actions when the specified element is clicked
 
 //------------------------------------------------------------------------------
 
@@ -17,37 +17,42 @@ addButton.on('click', function() {
     }
 })
 
-//------------------------------------------------------------------------------
-
-// Delete activity - a delegated event is used since activities are created 
-// afterwards
-$('#pending').on('click', function(e) {
-    console.log('event');
-    var target = $(e.target);
-    console.log(target);
-    // If a .btn-delete element or .fa-trash-o element is clicked
-    if (target.is('.btn-delete .fa-trash-o')) {
-        target.parents('li').remove();
-    }
-})
 
 //------------------------------------------------------------------------------
 
-// Complete activity:
 $('#pending').on('click', function(e) {
     var target = $(e.target);
     var activity, button;
+
+    // Delete activity: a delegated event is used since activities are created 
+    // afterwards.
+    // If a .btn-delete element or .fa-trash-o element is clicked
+    if (target.is('.btn-delete') || target.is('.fa-trash-o')) {
+        console.log("1");
+        target.parents('li').remove();
+    }
+
+    // Complete activity: a delegated event is used since activities are created 
+    // afterwards.
     // If a .btn-complete element or .fa-check element is clicked
-    if (target.is('.btn-complete .fa-check')) {
-        // if the target is the button
+    if (target.is('.btn-complete') || target.is('.fa-check')) {
+        // If the target is the button
         if (target.is('.btn-complete')) {
-            // hide 'check' button
-            target.css("display", "none");
+            console.log("2");
+            button = target;
+            // Hide 'check' button
+            button.css("display", "none");
+            // Add rounded corners to 'delete' button
+            button.siblings().addClass("rounded");
         }
-        // if the target is the font icon
+        // If the target is the font icon
         else {
-            // hide 'check' button
-            target.parent().css("display", "none");
+            console.log("3");
+            button = target.parent();
+            // Hide 'check' button
+            button.css("display", "none");
+            // Add rounded corners to 'delete' button
+            button.siblings().addClass("rounded");
         }
         // Select list item
         activity = target.parents('li');
@@ -63,15 +68,15 @@ $('#pending').on('click', function(e) {
 
 //------------------------------------------------------------------------------
 
-///Add item to the 'pending' list
+// Add item to the 'pending' list
 function addItemPending(text) {
     var pendingItem = '<li class="list-group-item list-group-item-warning list-group-item-action d-flex justify-content-between align-items-center rounded"> <p class="text-truncate">%data%</p><div class="btn-group" role="group" aria-label="functions"> <button type="button" class="btn-delete btn btn-danger" data-toggle="tooltip" data-placement="auto" title="Delete activity"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button> <button type="button" class="btn-complete btn btn-info"><i class="fa fa-2x fa-check" aria-hidden="true"></i></button> </div></li>';
     content = pendingItem.replace("%data%", text);
-    /* Latest activity first in the list */
+    // Latest activity should be on top
     $("#pending").prepend(content);
 }
 
-///Add item to the 'complete' list
+// Add item to the 'complete' list
 function addItemComplete(activity) {
     $("#complete").prepend(activity);
 }
