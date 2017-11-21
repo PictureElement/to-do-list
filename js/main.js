d4fc912ac820cc8ed7a37922a39a0b5ddad5770d
Marios Sofokleous <marios.sofokleous@yandex.com>
2017-11-21 17:58:01 +0200

    Add function that runs every time the 'data' object is manipulated

--- js/main.js
+++ js/main.js
@@ -25,12 +25,12 @@ addButton.on('click', function() {
 
 //------------------------------------------------------------------------------
 
-// Remove & Complete functionalities for the 'pending' list
+// Remove & Complete functionalities of the 'pending' list
 $('#pending').on('click', function(e) {
     var target = $(e.target);
     var activity, button, text;
 
-    // Remove activity: a delegated event is used since activities are created 
+    // Remove functionality: a delegated event is used since activities are created 
     // afterwards.
     // If a .btn-delete element or .fa-trash-o element is clicked
     if (target.is('.btn-delete') || target.is('.fa-trash-o')) {
@@ -40,11 +40,13 @@ $('#pending').on('click', function(e) {
         text = activity.text();
         // Remove item from data.pending
         data.pending.splice(data.pending.indexOf(text), 1);
+        // Update local storage
+        localStorageUpdate();
         // Remove item from the DOM
         activity.remove();
     }
 
-    // Complete activity: a delegated event is used since activities are created 
+    // Complete functionality: a delegated event is used since activities are created 
     // afterwards.
     // If a .btn-complete element or .fa-check element is clicked
     if (target.is('.btn-complete') || target.is('.fa-check')) {
@@ -64,7 +66,6 @@ $('#pending').on('click', function(e) {
         activity.remove();
         // Remove item from data.pending
         data.pending.splice(data.pending.indexOf(text), 1);
-        console.log(data);
         // Add item to the data.complete and to the DOM
         addItemComplete(text);
     }
@@ -72,12 +73,12 @@ $('#pending').on('click', function(e) {
 
 //------------------------------------------------------------------------------
 
-// Remove functionality for the 'complete' list
+// Remove functionality of the 'complete' list
 $('#complete').on('click', function(e) {
     var target = $(e.target);
     var activity, text;
-    // Remove activity: a delegated event is used since activities are created 
-    // afterwards.
+    // Remove functionality: a delegated event is used since activities are 
+    // created afterwards.
     // If a .btn-delete element or .fa-trash-o element is clicked
     if (target.is('.btn-delete') || target.is('.fa-trash-o')) {
         // Select list item
@@ -86,7 +87,8 @@ $('#complete').on('click', function(e) {
         text = activity.text();
         // Remove item from data.complete
         data.complete.splice(data.complete.indexOf(text), 1);
-        console.log(data);
+        // Update local storage
+        localStorageUpdate();
         // Remove item from the DOM
         activity.remove();
     }
@@ -94,11 +96,18 @@ $('#complete').on('click', function(e) {
 
 //------------------------------------------------------------------------------
 
+function localStorageUpdate() {
+    console.log("local storage updated");
+}
+
+//------------------------------------------------------------------------------
+
 // Add pending item
 function addItemPending(text) {
     // Add item to data.pending
     data.pending.push(text);
-    console.log(data);
+    // Update local storage
+    localStorageUpdate();
     var pendingItem = '<li class="list-group-item list-group-item-warning list-group-item-action d-flex justify-content-between align-items-center rounded"><p class="text-truncate">%data%</p><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger" data-toggle="tooltip" data-placement="auto" title="Delete activity"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button><button type="button" class="btn-complete btn btn-info"><i class="fa fa-2x fa-check" aria-hidden="true"></i></button></div></li>';
     content = pendingItem.replace("%data%", text);
     // Add activity to the DOM
@@ -111,7 +120,8 @@ function addItemPending(text) {
 function addItemComplete(text) {
     // Add item to data.complete
     data.complete.push(text);
-    console.log(data);
+    // Update local storage
+    localStorageUpdate();
     var completeItem = '<li class="list-group-item list-group-item-success list-group-item-action d-flex justify-content-between align-items-center rounded"><p class="text-truncate">%data%</p><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger" data-toggle="tooltip" data-placement="auto" title="Delete activity"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button></div></li>';
     content = completeItem.replace("%data%", text);
     // Add activity to the DOM
