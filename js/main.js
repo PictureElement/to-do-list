74ba331b6d19f581a9754e1177bde4fd84dd49d4
Marios Sofokleous <marios.sofokleous@yandex.com>
2017-11-27 18:20:29 +0200

    Necessary changes to main.js in order for the list to function properly with the extra input fields

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var data;
 
-// Load list from local storage (if it is possible) or as an empty object
-// getItem() returns null if the localStorage object is empty
+// Construct an empty JS object if the localStorage object is empty.
+// getItem() returns null if the localStorage object is empty.
 if (localStorage.getItem("listData") === null) {
     data = {
-        pending: [],
-        complete: []
+        "pending": [],
+        "complete": []
     };
 }
 else {
@@ -13,15 +13,15 @@ else {
     data = JSON.parse(localStorage.getItem("listData"));
 
     for (var i = 0; i < data.pending.length; i++) {
-        var text = data.pending[i];
+        var task = data.pending[i];
         // flag = 0: no need to update local storage and add to 'data'
-        addItemPending(text, 0);
+        addItemPending(task, 0);
     }
 
     for (var j = 0; j < data.complete.length; j++) {
-        var text = data.complete[j];
+        var task = data.complete[j];
         // flag = 0: no need to update local storage and add to 'data'
-        addItemComplete(text, 0)
+        addItemComplete(task, 0)
     }
 }
 
@@ -43,17 +43,24 @@ var timeField = $('#item-3');
 // Select the add button
 var addButton = $('#item-4');
 
-// Add activity
+// Add task
 // A direct-bound event is used
 addButton.on('click', function() {
+    
     var text = textField.val();
-    // If text is not empty, add it to data.pending and to the DOM
+    var date = dateField.val(); // 2017-11-28 (yyyy-mm-dd)
+    var time = timeField.val(); // 13:24 (24h format)
+    
+    // task object
+    var task = {"text":text, "date":date, "time":time, "htmlContent":""};
+    
+    // If text field is not empty, add user input to data.pending and to the DOM
     if (text) {
         // flag = 1: need to update local storage and add to 'data'
-        addItemPending(text, 1);
+        addItemPending(task, 1);
     }
     else {
-        alert("Please enter task")
+        alert("Please enter task");
     }
 })
 
@@ -61,27 +68,33 @@ addButton.on('click', function() {
 
 // Remove & Complete functionalities of the 'pending' list
 $('#pending').on('click', function(e) {
+    
     var target = $(e.target);
-    var activity, button, text;
+    var listItem, button, htmlContent, task, indexOfTask;
 
-    // Remove functionality: a delegated event is used since activities are created 
-    // afterwards.
+    // Remove functionality: a delegated event is used since activities are 
+    // created afterwards.
     // If a .btn-delete element or .fa-trash-o element is clicked
     if (target.is('.btn-delete') || target.is('.fa-trash-o')) {
         // Select list item
-        activity = target.parents('li');
-        // Get the text contents of the list item (i.e. actual user input)
-        text = activity.text();
-        // Remove item from data.pending
-        data.pending.splice(data.pending.indexOf(text), 1);
+        listItem = target.parents('li');
+        // Wrap list item in a dummy div tag in order to get the HTML contents
+        listItem.wrap("<div id='wrapper'></div>");
+        // Get the html contents of the list item
+        htmlContent = $("#wrapper").html();
+        // Remove the dummy div
+        listItem.unwrap();
+        // Remove list item from the DOM
+        listItem.remove();
+        // Remove task from data.pending
+        indexOfTask = data.pending.findIndex(i => i.htmlContent === htmlContent);
+        data.pending.splice(indexOfTask, 1);
         // Update local storage
         localStorageUpdate();
-        // Remove item from the DOM
-        activity.remove();
     }
 
-    // Complete functionality: a delegated event is used since activities are created 
-    // afterwards.
+    // Complete functionality: a delegated event is used since activities are 
+    // created afterwards.
     // If a .btn-complete element or .fa-check element is clicked
     if (target.is('.btn-complete') || target.is('.fa-check')) {
         // If the target is the button
@@ -93,16 +106,23 @@ $('#pending').on('click', function(e) {
             button = target.parent();
         }
         // Select list item
-        activity = target.parents('li');
-        // Get the text contents of the list item (i.e. actual user input)
-        text = activity.text();
-        // Remove activity from the DOM
-        activity.remove();
-        // Remove item from data.pending
-        data.pending.splice(data.pending.indexOf(text), 1);
-        // Add item to the data.complete and to the DOM
+        listItem = target.parents('li');
+        // Wrap list item in a dummy div tag in order to get the HTML contents
+        listItem.wrap("<div id='wrapper'></div>");
+        // Get the html contents of the list item
+        htmlContent = $("#wrapper").html();
+        // Remove the dummy div
+        listItem.unwrap();
+        // Remove list item from the DOM
+        listItem.remove();
+        // Remove task from data.pending
+        console.log(htmlContent);
+        indexOfTask = data.pending.findIndex(i => i.htmlContent === htmlContent);
+        task = data.pending[indexOfTask];
+        data.pending.splice(indexOfTask, 1);
+        // Add task to the data.complete and to the DOM
         // flag = 1: need to update local storage and add to 'data'
-        addItemComplete(text, 1);
+        addItemComplete(task, 1);
     }
 })
 
@@ -110,22 +130,27 @@ $('#pending').on('click', function(e) {
 
 // Remove functionality of the 'complete' list
 $('#complete').on('click', function(e) {
+    
     var target = $(e.target);
-    var activity, text;
+    var listItem, htmlContent, indexOfTask;
+    
     // Remove functionality: a delegated event is used since activities are 
     // created afterwards.
     // If a .btn-delete element or .fa-trash-o element is clicked
     if (target.is('.btn-delete') || target.is('.fa-trash-o')) {
         // Select list item
-        activity = target.parents('li');
-        // Get the text contents of the list item (i.e. actual user input)
-        text = activity.text();
+        listItem = target.parents('li');
+        // Wrap list item in a dummy div tag in order to get the HTML contents
+        listItem.wrap("<div id='wrapper'></div>");
+        // Get the html contents of the list item
+        htmlContent = $("#wrapper").html();
+        // Remove item from the DOM
+        listItem.remove();
         // Remove item from data.complete
-        data.complete.splice(data.complete.indexOf(text), 1);
+        indexOfTask = data.complete.findIndex(i => i.htmlContent === htmlContent);
+        data.complete.splice(indexOfTask, 1);
         // Update local storage
         localStorageUpdate();
-        // Remove item from the DOM
-        activity.remove();
     }
 })
 
@@ -135,13 +160,13 @@ $('#complete').on('click', function(e) {
 // will not be deleted when the browser is closed. 
 
 // Store:
-//localStorage.setItem("key", "value");
+// localStorage.setItem("key", "value");
 
 // Retrieve:
-//localStorage.getItem("key");
+// localStorage.getItem("key");
 
 // Remove:
-//localStorage.removeItem("key");
+// localStorage.removeItem("key");
 
 function localStorageUpdate() {
     // The localStorage object can only store text. Thus, we need to convert the 
@@ -152,34 +177,54 @@ function localStorageUpdate() {
 
 //------------------------------------------------------------------------------
 
-// Add pending item
-function addItemPending(text, flag) {
+// Add pending task
+function addItemPending(task, flag) {
+    
+    var pendingTask = '<li class="list-group-item list-group-item-warning list-group-item-action d-flex justify-content-between align-items-center rounded"><div><p class="text-truncate">%data1%</p><p class="secondary-text">%data2%, %data3%</p></div><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button><button type="button" class="btn-complete btn btn-info"><i class="fa fa-2x fa-check" aria-hidden="true"></i></button></div></li>';
+    
+    var htmlContent = pendingTask.replace("%data1%", task.text);
+    htmlContent = htmlContent.replace("%data2%", task.date);
+    htmlContent = htmlContent.replace("%data3%", task.time);
+
+    // Set the object's 'htmlContent' property
+    task.htmlContent = htmlContent;
+
+    console.log(htmlContent);
+
+    // Add task to the DOM
+    $("#pending").prepend(htmlContent);
+
     // flag = 1: need to update local storage and add to 'data'
     if (flag === 1) {
-        // Add item to data.pending
-        data.pending.push(text);
+        // Add task to data.pending
+        data.pending.push(task);
         // Update local storage
         localStorageUpdate();
     }
-    var pendingItem = '<li class="list-group-item list-group-item-warning list-group-item-action d-flex justify-content-between align-items-center rounded"><p class="text-truncate">%data%</p><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button><button type="button" class="btn-complete btn btn-info"><i class="fa fa-2x fa-check" aria-hidden="true"></i></button></div></li>';
-    content = pendingItem.replace("%data%", text);
-    // Add activity to the DOM
-    $("#pending").prepend(content);
 }
 
 //------------------------------------------------------------------------------
 
 // Add complete item
-function addItemComplete(text, flag) {
+function addItemComplete(task, flag) {
+
+    var completeTask = '<li class="list-group-item list-group-item-success list-group-item-action d-flex justify-content-between align-items-center rounded"><div><p class="text-truncate">%data1%</p><p class="secondary-text">%data2%, %data3%</p></div><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button></div></li>';
+    
+    var htmlContent = completeTask.replace("%data1%", task.text);
+    htmlContent = htmlContent.replace("%data2%", task.date);
+    htmlContent = htmlContent.replace("%data3%", task.time);
+
+    // Set the object's 'htmlContent' property
+    task.htmlContent = htmlContent;
+
+    // Add task to the DOM
+    $("#complete").prepend(htmlContent);
+
     // flag = 1: need to update local storage and add to 'data'
     if (flag === 1) {
-        // Add item to data.complete
-        data.complete.push(text);
+        // Add task to data.complete
+        data.complete.push(task);
         // Update local storage
         localStorageUpdate();
     }
-    var completeItem = '<li class="list-group-item list-group-item-success list-group-item-action d-flex justify-content-between align-items-center rounded"><p class="text-truncate">%data%</p><div class="btn-group" role="group" aria-label="functions"><button type="button" class="btn-delete btn btn-danger"><i class="fa fa-2x fa-trash-o" aria-hidden="true"></i></button></div></li>';
-    content = completeItem.replace("%data%", text);
-    // Add activity to the DOM
-    $("#complete").prepend(content);
 }
